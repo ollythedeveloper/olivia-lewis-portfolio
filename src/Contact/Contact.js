93909d8cd5776963a2f73e59922ae692f908bc1a
Olivia Lewis <ollybaby12@gmail.com>
2021-05-30 04:16:51 -0400

    correct typo

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -5,9 +5,9 @@ export default function Contact() {
     return (
         <div className="App__contact">
             <h2 className="cont-head">Contact</h2>
-            <div class="group">
-              <div class="item-double">
-              <form class="form-txt left-side" action="https://formspree.io/f/xbjpzbzz" method="post">
+            <div className="group">
+              <div className="item-double">
+              <form className="form-txt left-side" action="https://formspree.io/f/xbjpzbzz" method="post">
                <label htmlFor="user-name">Name:</label>
                <input id="user-name" type="text" name="name"/>
 
@@ -19,7 +19,7 @@ export default function Contact() {
                <button type="submit">Submit</button>
               </form>
             </div>
-              <div class="item left-side">
+              <div className="item left-side">
             <p>Check out my <a href="https://github.com/ollythedeveloper"> Github</a> and <a href="https://www.linkedin.com/in/olivia-lewis-10932183">LinkedIn</a>!</p><p>Contact me if you would like to collaborate! I look forward to hearing from you!</p>
             
             <p>Email: <a href="mailto:ollybaby12@gmail.com">ollybaby12@gmail.com</a></p>
